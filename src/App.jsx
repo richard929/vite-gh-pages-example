58f7ed0e31ebf2d034ae8d3250ce9feb0769a9e8
Richard Zhan <richard93929@gmail.com>
2023-05-01 11:24:47 -0400

    fix base path config

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 import { Example } from "./components/Example";
 
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
@@ -11,12 +11,14 @@ function App() {
       <Header />
       <div className="app">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/example" element={<Example />} />
+          <Route path="/vite-gh-pages-example">
+            <Route path="" element={<Home />} />
+            <Route path="example" element={<Example />} />
+          </Route>
         </Routes>
       </div>
     </Router>
   );
 }
 
-export default App
+export default App;
