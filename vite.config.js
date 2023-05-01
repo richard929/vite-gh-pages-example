cac1a04c360119631e4dc7523f14b8a8912a3ed3
Richard Zhan <richard93929@gmail.com>
2023-05-01 11:31:13 -0400

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-gh-pages-example',
+  base: '/vite-gh-pages-example/',
   plugins: [react()],
 })
